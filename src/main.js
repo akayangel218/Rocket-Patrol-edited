e4b685c8c104cdd3aa5c99fdc8c42ab42eb1489a
Kidus Michael <kimichae@ucsc.edu>
2021-06-28 23:10:16 -0700

    fin

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 /*Points breakdown:
 - Add your own (copyright-free) background music to the Play scene (5)
 - Allow the player to control the Rocket after it's fired (5)
+- Changed the Green Rectangle to Red for expert mode (???)
 - Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
 - Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (20) 
 - Implement a simultaneous two-player mode (30)
